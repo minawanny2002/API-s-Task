20b03ac5ac18923ae48a56418b38c10951b91acd
Mina Wanny <98766274+minawanny2002@users.noreply.github.com>
2023-04-24 02:15:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let baseURL = "https://api.weatherapi.com/v1/current.json?key=5ea1184c6fc344dd99712317232304&q=";
+let rest_of_URL = "&aqi=no";
+
+
+function for_button_click()
+{    let regions=[];
+     async function getWeather(endpoint)
+    {
+        let output = document.getElementById("out");
+        
+        let response = await fetch(`${baseURL}${endpoint}${rest_of_URL}`);
+        response = await response.json();
+        regions = response;
+
+        console.log(regions);
+
+
+        output.innerHTML = `
+        <h2>Name : ${regions.location.name}</h2>
+        <h2>Region : ${regions.location.region}</h2>
+        <h2>Country : ${regions.location.country}</h2>
+        <h2>Tempreature In Celcius : ${regions.current.temp_c} </h2>
+        <h2>Tempreature In Fehrenheit : ${regions.current.temp_f}</h2>
+
+        <div class="flex">
+        <h2 class="flex">${regions.current.condition.text}</h2>
+        <img class="flex" id="icon" src="${regions.current.condition.icon}" alt="" class="icon">
+        </div
+        
+                            `;
+    }
+    
+    
+    
+    
+    let user_choice = document.getElementById("user_input").value;
+    getWeather(user_choice);
+}; 
+    
+    
+    let btn = document.getElementById("button");
+    btn.onclick = for_button_click;
